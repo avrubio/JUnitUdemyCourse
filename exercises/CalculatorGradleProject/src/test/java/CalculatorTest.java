e9ffed37e22efbd069069ce81bd4e8423c2f7ea3
avrubio <ariadna.vanegas@gmail.com>
2024-06-29 12:03:57 -0400

    created integerSubtraction method and test

--- exercises/CalculatorGradleProject/src/test/java/CalculatorTest.java
+++ exercises/CalculatorGradleProject/src/test/java/CalculatorTest.java
@@ -14,4 +14,14 @@ class CalculatorTest {
         assertEquals(2, result, "Integer division did not produce expected result");
     }
 
+    @Test
+    void integerSubtraction(){
+        //Arrange
+        Calculator calculator= new Calculator();
+        //Act
+        int result = calculator.integerSubtraction(6,2);
+        //Assert
+        assertEquals(4, result, "Integer subtraction did not produce expected result");
+    }
+
 }
