6ef7af9856bffa0f1dab3e8e2b43dc27b79c87c9
avrubio <ariadna.vanegas@gmail.com>
2024-07-03 07:47:53 -0400

    starter code for ClassesOrderDemo

--- exercises/CalculatorGradleProject/src/test/java/MethodOrderedRandomlyTest.java
+++ exercises/CalculatorGradleProject/src/test/java/MethodOrderedRandomlyTest.java
@@ -1,25 +1,33 @@
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.Random.class)
+//@TestMethodOrder(MethodOrderer.Random.class)
+//@TestMethodOrder(MethodOrderer.MethodName.class) // sorts by the test method name in order, ABCD
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedRandomlyTest {
 
-    @Test
-    void TestA(){
-        System.out.println("Running Test A");
-    }
-    @Test
-    void TestB(){
-        System.out.println("Running Test B");
-    }
-    @Test
-    void TestC(){
-        System.out.println("Running Test C");
-    }
+    @Order(1)
     @Test
     void TestD(){
         System.out.println("Running Test D");
     }
+    @Order(2)
+    @Test
+    void TestA(){
+        System.out.println("Running Test A");
+    }
+    @Order(3)
+    @Test
+    void TestB(){
+        System.out.println("Running Test B");
+    }
+    @Order(4)
+    @Test
+    void TestC(){
+        System.out.println("Running Test C");
+    }
+
 
 }
