9cb6813496791aeb94950b555b76a20743cfdea8
avrubio <ariadna.vanegas@gmail.com>
2024-07-05 13:30:56 -0400

    TDD section, starter code

---
+++ exercises/UserService/src/test/java/UserServiceTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void testCreateUser_whenUserDetailsProvided_ReturnUserObject(){
+        //Arrange
+        //Act
+        //Assert
+    }
+}
