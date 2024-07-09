6d288b02a0def1dde334b8a55202fc4a1d4694cd
avrubio <ariadna.vanegas@gmail.com>
2024-07-09 07:54:31 -0400

    fixed failing tests after refactor'
    qa:
    '

--- exercises/UserService/src/test/java/com/invadethecode/service/UserServiceTest.java
+++ exercises/UserService/src/test/java/com/invadethecode/service/UserServiceTest.java
@@ -15,23 +15,24 @@ public class UserServiceTest {
     String email;
     String password;
     String repeatPassword;
+
     //Arrange for each test instance
     @BeforeEach
-    void init(){
-        UserService userService = new UserServiceImpl();
-        String firstName = "ari";
-        String lastName = "vanegas";
-        String email = "test@aol.com";
-        String password = "1234567";
-        String repeatPassword = "1234567";
+    void init() {
+        userService = new UserServiceImpl();
+        firstName = "ari";
+        lastName = "vanegas";
+        email = "test@aol.com";
+        password = "1234567";
+        repeatPassword = "1234567";
     }
 
     @DisplayName("User object created")
     @Test
-    void testCreateUser_whenUserDetailsProvided_ReturnUserObject(){
+    void testCreateUser_whenUserDetailsProvided_ReturnUserObject() {
 
         //Act
-        User user = userService.createUser(firstName,lastName,email,password,repeatPassword);
+        User user = userService.createUser(firstName, lastName, email, password, repeatPassword);
         //Assert
         assertNotNull(user, "The createUser() should not have returned null");
         assertEquals(firstName, user.getFirstName(), "users first name does not match");
@@ -42,11 +43,11 @@ public class UserServiceTest {
 
     @DisplayName("Empty first name causes correct exception")
     @Test
-    void testCreateUser_whenFirstNameIsEmpty_throwsIllegalArgumentException(){
-
+    void testCreateUser_whenFirstNameIsEmpty_throwsIllegalArgumentException() {
+ firstName = "";
         //Act & Assert
-       IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
-            userService.createUser(firstName,lastName,email,password,repeatPassword);
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            userService.createUser(firstName, lastName, email, password, repeatPassword);
         }, "Empty first name should have cause and Illegal Argument Exception");
         //Assert
         assertEquals("User's first name is empty", thrown.getMessage(),
